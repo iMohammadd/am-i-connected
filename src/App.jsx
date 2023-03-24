256ba1701319f830ea71f984fd7d7c930d8ea940
Mohammad Mohammadi <imohammadd@gmail.com>
2023-03-24 14:44:10 +0430

    init

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+import useSound from "use-sound"
+import beep from '/beep.mp3'
+
+const App = () => {
+  const [ip, setIp] = useState("")
+  const [time, setTime] = useState("")
+  const [error, setError] = useState(false)
+  const [play] = useSound(beep, {
+    volume: 1
+  })
+  const getIp = () => {
+    axios.get('https://ifconfig.me')
+    .then(response => {
+      setError(false)
+      setIp(response.data)
+    })
+    .catch(error => {
+      setError(true)
+      play()
+    })
+  }
+  useEffect(() => {
+    getIp()
+    setTime(new Date().toLocaleTimeString())
+
+    const timer = setInterval(() => setTime(new Date().toLocaleTimeString()), 1000)
+
+    const connection = setInterval(() => {
+      getIp()
+      console.log('code executed')
+    }, 60000)
+
+    return () => {
+      clearInterval(timer)
+      clearInterval(connection)
+    }
+  }, [])
+
+  return (
+    <div className={`${error ? 'bg-red-600' : 'bg-gray-800'} w-full h-screen flex justify-center items-center`}>
+      <h1 className={`text-3xl font-bold text-white`} onClick={() => play()} >{ip}    {time}</h1>
+      <button className=" rounded bg-indigo-500 text-white px-2 py-1 ml-6" onClick={() => getIp()}>Get IP</button>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'am i connected?',
+        short_name: 'am i connected?',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+})
